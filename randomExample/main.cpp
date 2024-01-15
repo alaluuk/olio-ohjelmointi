0fe4943579302bd67259d0a9571092cdd24f768f
Pekka Alaluukas <pekka.alaluukas@oamk.fi>
2024-01-15 08:19:45 +0200

    satunnaisluku esimerkki valmis

--- randomExample/main.cpp
+++ randomExample/main.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    //sovellus tulostaa satunnaisluvun
+    //väliltä 1-10
+    srand(time(0));
+    int myRandom=rand()%10 +1;
+    cout<<"Satunnaisluku = "<<myRandom<<endl;
     return 0;
 }
