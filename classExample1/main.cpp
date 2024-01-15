e06c554e9a42fdc3445a577b538a3bfd2b715898
Pekka Alaluukas <pekka.alaluukas@oamk.fi>
2024-01-15 09:35:07 +0200

    classExample1 aloitettu

--- classExample1/main.cpp
+++ classExample1/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
 using namespace std;
-
+//esimerkki Student luokasta, jolla
+//private tyyppiset jäsenmuuttujat fname, lname
+//ja niille public tyyppiset Getter ja Setter metodit
+//sekä public tyyppinen metodi StudentInfo, joka tulostaa
+//tiedot
 int main()
 {
     cout << "Hello World!" << endl;
