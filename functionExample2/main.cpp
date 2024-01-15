f2737ed1358e7a668de92b3a51a4046e91696eb2
Pekka Alaluukas <pekka.alaluukas@oamk.fi>
2024-01-15 08:41:30 +0200

    funktionExample2 aloitettu

---
+++ functionExample2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+//kirjoita funktio, joka ottaa vastaan kaksi kokonaislukua
+//funktio kertoo molemmat luvut kahdella ja tulostaa summan
+//tulosta myös alkuperäiset luvut
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
