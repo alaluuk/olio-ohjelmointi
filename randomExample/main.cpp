c781eb0d86ee44eb15d00b451dd337066b9d5844
Pekka Alaluukas <pekka.alaluukas@oamk.fi>
2024-01-15 08:20:18 +0200

    satunnaisluku esimerkki valmis

--- randomExample/main.cpp
+++ randomExample/main.cpp
@@ -6,8 +6,9 @@ int main()
 {
     //sovellus tulostaa satunnaisluvun
     //väliltä 1-10
-    srand(time(0));
-    int myRandom=rand()%10 +1;
+
+    srand(time(0)); //asetetaan siemenluku kellonajan avulla
+    int myRandom=rand()%10 +1; //jakojäännös jaettaessa 10:lla +1
     cout<<"Satunnaisluku = "<<myRandom<<endl;
     return 0;
 }
