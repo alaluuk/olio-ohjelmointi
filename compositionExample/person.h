011f591ed07cb59ba1e5cf81c7c0f872657e2d88
Pekka Alaluukas <pekka.alaluukas@oamk.fi>
2024-01-29 09:07:45 +0200

    koosteesimerkki valmis

---
+++ compositionExample/person.h
@@ -0,0 +1,27 @@
+#ifndef PERSON_H
+#define PERSON_H
+#include "cat.h"
+
+#include<iostream>
+
+using namespace std;
+class Person
+{
+private:
+    string fname;
+    string lname;
+    Cat objectCat1;
+    //luodaan Cat luokan olio
+    //eli kyseessä vahva kooste=composition
+public:
+    Person();
+    Person(string fn, string ln);
+    ~Person();
+    void setFname(const string &newFname);
+    void setLname(const string &newLname);
+    void personInfo();
+    void setCatData();
+
+};
+
+#endif // PERSON_H
